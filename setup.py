4270e0f747b145cdeec3f57dccf0330501e65625
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2019-07-06 01:57:01 +0500

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='CopyCatch',
+    version='0.1',
+    author='elcid',
+    author_email='syedmohsinbukhari@googlemail.com',
+    maintainer='ElCid',
+    maintainer_email='syedmohsinbukhari@googlemail.com',
+    url='https://github.com/syedmohsinbukhari/CopyCatch',
+    license="MIT",
+    description='CopyCatch (CopyCatch: Stopping Group Attacks by Spotting Lockstep Behavior in Social Networks Python3 implementation',
+    long_description='',
+    packages=[],
+    classifiers=[]
+)
+
